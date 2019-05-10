6eb689678e243681d8251a43542b324b7ae6fb68
blubrandi <blubrandi0@gmail.com>
2019-05-10 11:09:46 -0600

    Created NavLinks to /register, /login, and /jokes on App.js with a logout option.

--- brandi/src/App.js
+++ brandi/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
+      <header>
+        <NavLink to='/register'>Register | </NavLink>
+        <NavLink to='/login'>Login | </NavLink>
+        <NavLink to='/jokes'>Jokes | </NavLink>
+        {/* <div onClick={logout}>Logout</div> */}
       </header>
     </div>
   );
