d29e4038242df070b33e046532b809d9a9cd135e
blubrandi <blubrandi0@gmail.com>
2019-05-10 12:22:21 -0600

    Added header styles within app.js.  Added Home Link.  Imported noramlize.css in app.css

--- brandi/src/App.js
+++ brandi/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { Route, NavLink, withRouter } from 'react-router-dom'
-import './App.css';
+import styled from 'styled-components'
+import './App.css'
 
 import Register from './components/Register'
 import Login from './components/Login'
 import Jokes from './components/Jokes'
 
+const AppContainer = styled.div`
+  font-family: sans-serif;
+
+  header {
+    background-color: #333;
+    width: 100%;
+    color: #fff;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    height: 60px;
+
+    a, .Link {
+      color: #fff;
+      text-decoration: none;
+      padding-right: 15px;
+      cursor: pointer;
+    }
+  }
+`
 
 function App(props) {
 
@@ -16,17 +37,20 @@ function App(props) {
 
   return (
     <div className="App">
-      <header>
-        <NavLink to='/register'>Register | </NavLink>
-        <NavLink to='/login'>Login | </NavLink>
-        <NavLink to='/jokes'>Jokes | </NavLink>
-        <div onClick={logout}>Logout</div>
-      </header>
-      <main>
-        <Route path='/register' component={Register} />
-        <Route path='/login' component={Login} />
-        <Route path='/jokes' component={Jokes} />
-      </main>
+      <AppContainer>
+        <header>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/register'>Register</NavLink>
+          <NavLink to='/login'>Login</NavLink>
+          <NavLink to='/jokes'>Jokes</NavLink>
+          <div className="Link" onClick={logout}>Logout</div>
+        </header>
+        <main>
+          <Route path='/register' component={Register} />
+          <Route path='/login' component={Login} />
+          <Route path='/jokes' component={Jokes} />
+        </main>
+      </AppContainer>
     </div>
   );
 }
