e8f6088799e3bc0e55f3b41c25aa8755c9dcded7
blubrandi <blubrandi0@gmail.com>
2019-05-10 11:13:27 -0600

    Created registration form in Register.js

---
+++ brandi/src/components/Register.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import axios from 'axios';
+
+class Register extends React.Component {
+    state = {
+        username: '',
+        password: '',
+    };
+
+    render() {
+        return (
+            <>
+                <h2>Register</h2>
+                <form onSubmit={this.submitForm}>
+                    <div>
+                        <label htmlFor="username" />
+                        <input
+                            id="username"
+                            onChange={this.handleChange}
+                            value={this.state.username}
+                            type="text"
+                            placeholder="Username"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" />
+                        <input
+                            id="password"
+                            onChange={this.handleChange}
+                            value={this.state.password}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
+            </>
+        );
+    }
+
+    handleChange = event => {
+        const { id, value } = event.target;
+
+        this.setState({ [id]: value });
+    };
+
+    submitForm = event => {
+        event.preventDefault();
+        const endpoint = 'http://localhost:3300/api/auth/register';
+
+        axios
+            .post(endpoint, this.state)
+            .then(res => {
+                localStorage.setItem('jwt', res.data.token);
+                this.props.history.push('/jokes');
+            })
+            .catch(err => {
+                console.error('Registration Error', err);
+            });
+    };
+}
+
+export default Register;
